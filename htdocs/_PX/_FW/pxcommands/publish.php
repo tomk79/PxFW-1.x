4e644dbe4a9de44c04f73e2c291afa078fb61e7c
Tomoya Koyanagi <tomk79@gmail.com>
2012-03-24 13:15:42 +0900

    update publish.php : 途中だけど。 added spacer.gif: 一旦。
    Signed-off-by: Tomoya Koyanagi <tomk79@gmail.com>

--- htdocs/_PX/_FW/pxcommands/publish.php
+++ htdocs/_PX/_FW/pxcommands/publish.php
@@ -9,6 +9,10 @@ class px_pxcommands_publish extends px_bases_pxcommand{
 
 	protected $pxcommand_name = 'publish';
 
+	private $path_docroot_dir;
+	private $path_publish_dir;
+	private $paths_ignore_dir = array();
+
 	public function __construct( &$px ){
 		parent::__construct( &$px );
 		$this->execute();
@@ -20,12 +24,37 @@ class px_pxcommands_publish extends px_bases_pxcommand{
 	 * @return null
 	 */
 	private function execute(){
+		$this->setup();
 		@header('Content-type: text/plain');
 		print 'publish | Pickles Framework'."\n";
 		print '------'."\n";
 		print 'PX command "'.$this->pxcommand_name.'" executed.'."\n";
+		print '------'."\n";
+		print 'path_docroot_dir => '.$this->path_docroot_dir."\n";
+		print 'path_publish_dir => '.$this->path_publish_dir."\n";
+		print 'paths_ignore_dir => '."\n";
+		var_dump($this->paths_ignore_dir);
+		print '------'."\n";
+		if(!is_dir($this->path_docroot_dir)){
+			print 'path_docroot_dir is NOT exists.'."\n";
+			exit;
+		}
+		if(!is_dir($this->path_publish_dir)){
+			print 'path_publish_dir is NOT exists.'."\n";
+			exit;
+		}
 		print 'under construction.'."\n";
 		exit;
+	}
+	/**
+	 * セットアップ
+	 * @return true
+	 */
+	private function setup(){
+		$this->path_docroot_dir = realpath('.');
+		$this->path_publish_dir = realpath($this->px->get_conf('paths.publish_dir'));
+		array_push( $this->paths_ignore_dir , realpath($this->px->get_conf('paths.px_dir')) );
+		return true;
 	}
 }
 ?>
