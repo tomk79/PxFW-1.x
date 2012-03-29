2bdfa80486e5e6784254361beb9c36ef6c2d4d52
Tomoya Koyanagi <tomk79@gmail.com>
2012-03-29 15:55:14 +0900

    PX command "pageinfo" の入れ物を一旦実装。
    Signed-off-by: Tomoya Koyanagi <tomk79@gmail.com>

---
+++ htdocs/_PX/_FW/pxcommands/pageinfo.php
@@ -0,0 +1,28 @@
+<?php
+$this->load_pxclass('/bases/pxcommand.php');
+
+/**
+ * PX Command: pageinfoを表示する
+ **/
+class px_pxcommands_pageinfo extends px_bases_pxcommand{
+
+	protected $pxcommand_name = 'pageinfo';
+
+	public function __construct( &$px ){
+		parent::__construct( &$px );
+		$this->execute();
+	}//__construct()
+
+	/**
+	 * Execute PX Command "pageinfo".
+	 */
+	private function execute(){
+		$pageinfo = $this->px->site()->get_page_info( $this->px->req()->get_request_file_path() );
+		ob_start();
+		test::var_dump( $pageinfo );
+		$src = ob_get_clean();
+		print $this->html_template( $src );
+		exit;
+	}
+}
+?>
