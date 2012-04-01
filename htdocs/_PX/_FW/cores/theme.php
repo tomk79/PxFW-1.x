b990f3171f4330634e6f330183083551bc106482
Tomoya Koyanagi <tomk79@gmail.com>
2012-04-01 12:22:46 +0900

    パンくずを表示する機能を追加。 $theme->mk_breadcrumb()
    Signed-off-by: Tomoya Koyanagi <tomk79@gmail.com>

--- htdocs/_PX/_FW/cores/theme.php
+++ htdocs/_PX/_FW/cores/theme.php
@@ -84,8 +84,9 @@ class px_cores_theme{
 	}
 
 	/**
-	 * リンクタグを生成する。
+	 * リンクタグ(aタグ)を生成する。
 	 * @param $linkto リンク先のパス。PxFWのインストールパスを基点にした絶対パスで指定。
+	 * @param options: [as string] Link label, [as array] Any options.
 	 * @return string aタグ
 	 */
 	public function mk_link( $linkto ){
@@ -110,7 +111,38 @@ class px_cores_theme{
 		return $rtn;
 	}
 
-
+	/**
+	 * パンくずのHTMLソースを生成する。
+	 * @return HTML source
+	 */
+	public function mk_breadcrumb(){
+		$args = func_get_args();
+		$current_path = $this->px->req()->get_request_file_path();
+		if(strlen($args[0])){
+			//オプションで指定があれば、カレントページを仮定する。
+			$current_path = $args[0];
+		}
+		$page_info = $this->px->site()->get_page_info($current_path);
+		$page_info['logical_path'] = trim($page_info['logical_path']);
+		if( $page_info['id'] == '' ){
+			//  ホームの場合
+			return '<ul><li><strong>'.t::h($this->px->site()->get_page_info('','title_breadcrumb')).'</strong></li></ul>';
+		}
+		$array_breadcrumb = explode('>',$page_info['logical_path']);
+		if( !strlen( $page_info['logical_path'] ) ){
+			$array_breadcrumb = array();
+		}
+		$rtn = '';
+		$rtn .= '<ul>';
+		$rtn .= '<li><a href="'.t::h($this->href('')).'">'.t::h($this->px->site()->get_page_info('','title_breadcrumb')).'</a></li>';
+		foreach( $array_breadcrumb as $page_id ){
+			$linkto_page_info = $this->px->site()->get_page_info($page_id);
+			$rtn .= '<li> &gt; <a href="'.t::h($this->href($linkto_page_info['path'])).'">'.t::h($linkto_page_info['title_breadcrumb']).'</a></li>';
+		}
+		$rtn .= '<li> &gt; <strong>'.t::h($page_info['title_breadcrumb']).'</strong></li>';
+		$rtn .= '</ul>';
+		return $rtn;
+	}
 
 }
 ?>
