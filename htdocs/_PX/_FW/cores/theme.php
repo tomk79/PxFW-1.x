380ca673b36c9b8264b66a85ffc6f200f5566fb8
Tomoya Koyanagi <tomk79@gmail.com>
2012-04-01 11:37:34 +0900

    $theme->mk_link() 追加、common.cssとりあえず追加、他テーマに関連する修正。
    Signed-off-by: Tomoya Koyanagi <tomk79@gmail.com>

--- htdocs/_PX/_FW/cores/theme.php
+++ htdocs/_PX/_FW/cores/theme.php
@@ -10,24 +10,36 @@ class px_cores_theme{
 
 	/**
 	 * テーマIDをセットする。
+	 * @return bool
 	 */
 	public function set_theme_id( $theme_id ){
 		if( !strlen( $theme_id ) ){ return false; }
+		if( !preg_match( '/^[a-zA-Z0-9\_\-]+$/si' , $theme_id ) ){ return false; }
 		$this->theme_id = $theme_id;
 		return true;
 	}
+	/**
+	 * テーマIDを取得する。
+	 * @return string
+	 */
 	public function get_theme_id(){
 		return $this->theme_id;
 	}
 
 	/**
 	 * レイアウトIDをセットする。
+	 * @return bool
 	 */
 	public function set_layout_id( $layout_id ){
 		if( !strlen( $layout_id ) ){ return false; }
+		if( !preg_match( '/^[a-zA-Z0-9\_\-]+$/si' , $layout_id ) ){ return false; }
 		$this->layout_id = $layout_id;
 		return true;
 	}
+	/**
+	 * レイアウトIDを取得する。
+	 * @return string
+	 */
 	public function get_layout_id(){
 		return $this->layout_id;
 	}
@@ -61,11 +73,44 @@ class px_cores_theme{
 		return $src;
 	}//bind_contents();
 
+	/**
+	 * リンク先を調整する。
+	 * @return string href属性値
+	 */
 	public function href( $linkto ){
 		$path = $this->px->site()->get_page_info($linkto,'path');
 		$path = preg_replace( '/^\/+/' , '' , $path );
-		return $this->px->site()->get_path_home().$path;
+		return $this->px->get_install_path().$path;
 	}
+
+	/**
+	 * リンクタグを生成する。
+	 * @param $linkto リンク先のパス。PxFWのインストールパスを基点にした絶対パスで指定。
+	 * @return string aタグ
+	 */
+	public function mk_link( $linkto ){
+		$args = func_get_args();
+		$href = $this->href($linkto);
+		$hrefc = $this->href($this->px->req()->get_request_file_path());
+		$label = $this->px->site()->get_page_info($linkto,'title_label');
+		if( is_string($args[1]) ){
+			//  第2引数が文字列なら
+			//  リンクのラベルとして採用
+			$label = $args[1];
+		}
+		if( count($args) >= 2 && is_array($args[count($args)-1]) ){
+			//  最後の引数が配列なら
+			//  オプション連想配列として読み込む
+			$options = $args[count($args)-1];
+			if(strlen($options['label'])){
+				$label = $options['label'];
+			}
+		}
+		$rtn = '<a href="'.t::h($href).'"'.($href==$hrefc?' class="current"':'').'>'.t::h($label).'</a>';
+		return $rtn;
+	}
+
+
 
 }
 ?>
