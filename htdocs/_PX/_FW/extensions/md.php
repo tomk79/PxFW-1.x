894bf969f645beffa950355874b161360f00cfcd
Tomoya Koyanagi <tomk79@gmail.com>
2013-10-24 22:40:28 +0900

    extension *.md で、PHPを書けるようにした。PxFW のAPIにアクセスできるようになった。
    - 実装としては、*.md のパース後のソースをキャッシュし、include() で読み込むように変更。
    - キャッシュは、タイムスタンプでオリジナルのコンテンツファイルと比較され、キャッシュの方が古い場合には再パースを行わない。

--- htdocs/_PX/_FW/extensions/md.php
+++ htdocs/_PX/_FW/extensions/md.php
@@ -13,13 +13,29 @@ class px_extensions_md extends px_bases_extension{
 	public function execute( $path_content ){
 		@header('Content-type: text/html; charset=UTF-8');//デフォルトのヘッダー
 
-		$src = @file_get_contents( $path_content );
+		$path_cache = $this->px->get_conf('paths.px_dir').'_sys/caches/contents/'.urlencode($path_content);
+		if(!is_dir(dirname($path_cache))){
+			$this->px->dbh()->mkdir( dirname($path_cache) );
+		}
 
-		//  PHP Markdownライブラリをロード
-		//  see: http://michelf.ca/projects/php-markdown/
-		@require_once( $this->px->get_conf('paths.px_dir').'libs/PHPMarkdown/markdown.php' );
+		if( !is_file( $path_cache ) || $this->px->dbh()->is_newer_a_than_b( $path_content, $path_cache ) ){
+			// キャッシュがない、またはオリジナルコンテンツよりも古い場合
+			$src = @file_get_contents( $path_content );
 
-		$src = Markdown($src);
+			//  PHP Markdownライブラリをロード
+			//  see: http://michelf.ca/projects/php-markdown/
+			@require_once( $this->px->get_conf('paths.px_dir').'libs/PHPMarkdown/markdown.php' );
+
+			$src = Markdown($src);
+			$this->px->dbh()->file_overwrite($path_cache, $src);
+		}
+
+		$src = '';
+		ob_start();
+		$px = $this->px;
+		include( $path_cache );
+		$src = ob_get_clean();
+
 		$src = $this->px->theme()->bind_contents( $src );
 		$src = $this->px->theme()->output_filter($src, 'html');
 		print $src;
